8d8dd628dc2fbf1eafb509065159be68c25f8aa0
jswilson <js.wilson@ymail.com>
2022-10-05 13:13:41 -0700

    make tensor

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -740,6 +740,7 @@ class LatentDiffusion(DDPM):
                 else:
                     print('the problem:')
                     print(xc)
+                    c = self.get_learned_conditioning(torch.tensor([xc]).to(self.device))
                     # c = self.get_learned_conditioning(xc.to(self.device))
                     c = self.get_learned_conditioning(xc)
             else:
