b65c0e650808536cd165e937b6375260e0df3895
jswilson <js.wilson@ymail.com>
2022-10-05 12:44:17 -0700

    debug

--- ldm/modules/diffusionmodules/model.py
+++ ldm/modules/diffusionmodules/model.py
@@ -436,9 +436,9 @@ class Encoder(nn.Module):
         temb = None
 
         # downsampling
-        print(x)
         print(x.shape)
         torch.permute(x, (1, 3, 0, 2))
+        print(x.shape)
         hs = [self.conv_in(x)]
         for i_level in range(self.num_resolutions):
             for i_block in range(self.num_res_blocks):
